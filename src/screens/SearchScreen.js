f93a7a61dd5923d68ffac2eac25fb39af66f25aa
Jason Crawford <daxapps777@gmail.com>
2019-08-25 06:51:16 -0400

    add search bar with error handling

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from '../components/SearchBar';
+import yelp from '../api/yelp';
+
+const SearchScreen = () => {
+	const [ term, setTerm ] = useState('');
+	const [ results, setResults ] = useState([]);
+	const [ errorMessage, setErrorMessage ] = useState('');
+
+	const searchApi = async () => {
+		try {
+			const response = await yelp.get('/search', {
+				params: {
+					limit: 50,
+					term,
+					location: 'san jose'
+				}
+			});
+			setResults(response.data.businesses);
+		} catch (err) {
+			setErrorMessage('Try Again');
+		}
+	};
+
+	return (
+		<View>
+			<SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+			{errorMessage ? <Text>{errorMessage}</Text> : null}
+			<Text>results: {results.length}</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
